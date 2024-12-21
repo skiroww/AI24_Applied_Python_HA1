8d685fc22a6584bc04f2444baafb37eb10f4e0a3
skiroww <45658452+skiroww@users.noreply.github.com>
2024-12-22 01:35:34 +0300

    Update test_frontend_streamlit.py

--- test_frontend_streamlit.py
+++ test_frontend_streamlit.py
@@ -148,5 +148,5 @@ def main():
         st.pyplot(map_fig)
 
 if __name__ == "__main__":
-    st.video('https://www.youtube.com/watch?v=NW2FMQPKNSs', loop=True)
+    st.video('https://yandex.ru/video/preview/4649117928571699222', loop=True)
     main()
